cd7922d637eab27e49e84f0b1fcd8922e4381094
Sachin Kumar <sachinkumar@Sachins-MacBook-Air.local>
2025-11-25 15:52:02 +0530

    Web app code done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sachin Kumar",
+    author_email="sachinkr7427@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
